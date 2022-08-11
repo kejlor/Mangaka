599a246e2afe207e8f41d0943406baaa0d4b795f
kejlor <woj450@gmail.com>
2022-08-11 17:59:54 +0200

    Initial Commit

---
+++ Mangaka/MangakaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangakaApp.swift
+//  Mangaka
+//
+//  Created by Bartosz Wojtkowiak on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MangakaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
